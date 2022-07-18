d88b7df86023bc8d7a0178ec9af84d2fe72b81de
mskora <mskora@deloittece.com>
2022-07-18 11:07:33 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Container animalContainer = new Container();
+        Container mammalContainer = new Container();
+        Container dogContainer = new Container();
+    }
+}
---
+++ src/main/java/org/example/animal/Dog.java
@@ -0,0 +1,5 @@
+package org.example.animal;
+
+public class Dog extends Mammal {
+
+}
